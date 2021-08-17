5bf1a8ae83bac8f77e6681cd8600768f7c798f12
a.shoeibi <a.shoeibi@alibaba.ir>
2021-08-18 03:47:13 +0430

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.NetworkInformation;
+
+using CancellationTokenSource cancellationTokenSource = new();
+CancellationToken cancellationToken = cancellationTokenSource.Token;
+
+Ping pingSender = new();
+
+using CancellationTokenRegistration cancellationTokenRegistration =
+                                    cancellationToken.Register(() => pingSender.SendAsyncCancel());
+
+int i = 0;
+while (true)
+{
+    cancellationToken.ThrowIfCancellationRequested();
+
+    PingReply pingReply = await pingSender.SendPingAsync("google.com");
+    Console.Write($"Status: {pingReply.Status}\t");
+    Console.Write($"RoundtripTime: {pingReply.RoundtripTime}\t");
+    Console.Write($"IP Address: {pingReply.Address}\t");
+    Console.Write($"TTL: {pingReply.Options.Ttl}\t");
+    Console.WriteLine($"DontFragment: {pingReply.Options.DontFragment}\t");
+    if (i == 0)
+    {
+        cancellationTokenSource.CancelAfter(TimeSpan.FromSeconds(10)); // This is not blocking call
+    }
+    i++;
+}
